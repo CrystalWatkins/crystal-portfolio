7d0c68028cdea6dfee524747743680e3f20eee04
Crystal <crystalwatkins428@gmail.com>
2020-09-25 11:17:13 -0400

    updated process.ENV email and password

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,8 +8,8 @@ const sendEmail = (from, subject, text) => {
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "crystalwatkins428@gmail.com",
-        pass: "pxctjzrxtejutfmj"
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
     }
 });
 
